1e5495a952e6e78ba9b5499aa3be215173deaaca
Peter Sýs <psppszavinacmnenepis@gmail.com>
2018-03-19 13:00:16 +0100

    Usable state

---
+++ src/main/java/Encryption.java
@@ -0,0 +1,128 @@
+import org.whispersystems.libsignal.*;
+import org.whispersystems.libsignal.ecc.Curve;
+import org.whispersystems.libsignal.ecc.ECPublicKey;
+import org.whispersystems.libsignal.protocol.PreKeySignalMessage;
+import org.whispersystems.libsignal.protocol.SignalMessage;
+import org.whispersystems.libsignal.state.PreKeyBundle;
+import org.whispersystems.libsignal.state.PreKeyRecord;
+import org.whispersystems.libsignal.state.SignalProtocolStore;
+import org.whispersystems.libsignal.state.SignedPreKeyRecord;
+import org.whispersystems.libsignal.util.KeyHelper;
+import org.whispersystems.libsignal.util.Medium;
+
+import java.util.*;
+
+public class Encryption {
+    private SignalProtocolStore store;
+    private SignalProtocolAddress myAddress;
+
+    private static Map<SignalProtocolAddress, IdentityKey> identityKeys = new HashMap<SignalProtocolAddress, IdentityKey>();
+    private static Map<SignalProtocolAddress, Pair<Integer,Pair<ECPublicKey,byte[]>>> signedKeys = new HashMap<SignalProtocolAddress, Pair<Integer, Pair<ECPublicKey, byte[]>>>();
+    private static Map<SignalProtocolAddress, List<PreKeyBundle>> oneTimeKeys = new HashMap<SignalProtocolAddress, List<PreKeyBundle>>();
+
+    public Encryption(SignalProtocolAddress myAdress) {
+        store = new MySignalProtocolStore();
+        this.myAddress = myAdress;
+    }
+
+    public void sendIdentityKey() {
+        //Needs to be called install time
+        sendIdentityKey(store.getIdentityKeyPair().getPublicKey());
+    }
+
+    public void rotateSingnedPreKey() throws InvalidKeyException {
+        //Needs to be called periodicly
+        int prekeyId;
+        do {
+            prekeyId = new Random().nextInt(Medium.MAX_VALUE);
+        } while (store.containsSignedPreKey(prekeyId));
+
+        SignedPreKeyRecord signedPreKey  = KeyHelper.generateSignedPreKey(store.getIdentityKeyPair(), prekeyId);
+        store.storeSignedPreKey(prekeyId,signedPreKey);
+        sendSignedPrekey(prekeyId, signedPreKey.getKeyPair().getPublicKey(), Curve.calculateSignature(store.getIdentityKeyPair().getPrivateKey(), signedPreKey.getKeyPair().getPublicKey().serialize()));
+    }
+
+    public void addOnetimePreKeys(int numberOfNewKeys) {
+        //Needs to be called when server lack of one time passwords
+        List<Pair<Integer,ECPublicKey>> toSend = new LinkedList<Pair<Integer, ECPublicKey>>();
+        for (int i = 0; i < numberOfNewKeys; i++) {
+            int prekeyId;
+            do {
+                prekeyId = new Random().nextInt(Medium.MAX_VALUE);
+            } while (store.containsPreKey(prekeyId));
+
+            PreKeyRecord preKey = new PreKeyRecord(prekeyId, Curve.generateKeyPair());
+            store.storePreKey(prekeyId, preKey);
+            toSend.add(new Pair<Integer, ECPublicKey>(prekeyId, preKey.getKeyPair().getPublicKey()));
+        }
+        sendPrekey(toSend);
+    }
+
+    public byte[] encrypt(SignalProtocolAddress address, byte[] message) throws UntrustedIdentityException, InvalidKeyException {
+        if (!store.containsSession(address)) {
+            SessionBuilder sessionBuilder = new SessionBuilder(store, address);
+            sessionBuilder.process(getPrekeyBundle(address));
+        }
+        SessionCipher sessionCipher = new SessionCipher(store, address);
+
+        return sessionCipher.encrypt(message).serialize();
+    }
+
+    public byte[] decrypt(SignalProtocolAddress address, byte[] message) throws LegacyMessageException, DuplicateMessageException, NoSessionException, UntrustedIdentityException, InvalidVersionException, InvalidKeyException, InvalidKeyIdException, InvalidMessageException {
+        SessionCipher sessionCipher = new SessionCipher(store, address);
+
+        try {
+            PreKeySignalMessage parsedMessage = new PreKeySignalMessage(message);
+            return sessionCipher.decrypt(parsedMessage);
+        } catch (InvalidMessageException e) {
+            SignalMessage parsedMessage = new SignalMessage(message);
+            return sessionCipher.decrypt(parsedMessage);
+        }
+    }
+
+    private PreKeyBundle getPrekeyBundle(SignalProtocolAddress address) {
+        //TODO vratit so serveru
+
+
+        return oneTimeKeys.get(address).remove(0);
+    }
+
+    private void sendIdentityKey(IdentityKey key) {
+        //TODO implement me
+        System.out.printf("Sending identity key to server\n");
+
+        assert (!identityKeys.containsKey(myAddress));
+        identityKeys.put(myAddress, key);
+    }
+
+    private void sendSignedPrekey(Integer i, ECPublicKey key, byte[] signature) throws InvalidKeyException {
+        //TODO implement me
+        System.out.printf("Sending signed pre key to server\n");
+
+        if (signedKeys.containsKey(myAddress))
+            signedKeys.remove(myAddress);
+        Pair<ECPublicKey,byte[]> vnutorny = new Pair<ECPublicKey,byte[]>(key, signature);
+        signedKeys.put(myAddress, new Pair<Integer,Pair<ECPublicKey,byte[]>>(i,vnutorny));
+    }
+
+    private void sendPrekey(List<Pair<Integer,ECPublicKey>> keys) {
+        //TODO implement me
+        System.out.printf("Sending signed pre key to server\n");
+
+        List<PreKeyBundle> to_add = new LinkedList<PreKeyBundle>();
+
+        Pair<Integer,Pair<ECPublicKey,byte[]>> signed = signedKeys.get(myAddress);
+
+        for (Pair<Integer,ECPublicKey> key : keys) {
+            PreKeyBundle n = new PreKeyBundle(store.getLocalRegistrationId(), myAddress.getDeviceId(), key.getLeft(), key.getRight(), signed.getLeft(), signed.getRight().getLeft(), signed.getRight().getRight(), store.getIdentityKeyPair().getPublicKey());
+             to_add.add(n);
+        }
+
+        if (oneTimeKeys.containsKey(myAddress)) {
+            oneTimeKeys.get(myAddress).addAll(to_add);
+        }
+        else {
+            oneTimeKeys.put(myAddress, to_add);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,83 +16,41 @@ import java.util.Random;
 
 
 public class Main {
-    private static final SignalProtocolAddress BOB_ADDRESS   = new SignalProtocolAddress("+14151231234", 1);
-    private static final SignalProtocolAddress ALICE_ADDRESS = new SignalProtocolAddress("+14159998888", 1);
-
-    private static final ECKeyPair aliceSignedPreKey = Curve.generateKeyPair();
-    private static final ECKeyPair bobSignedPreKey   = Curve.generateKeyPair();
-
-    private static final int aliceSignedPreKeyId = new Random().nextInt(Medium.MAX_VALUE);
-    private static final int bobSignedPreKeyId   = new Random().nextInt(Medium.MAX_VALUE);
-
+    private static final SignalProtocolAddress BOB_ADDRESS   = new SignalProtocolAddress("Bobik", 1);
+    private static final SignalProtocolAddress ALICE_ADDRESS = new SignalProtocolAddress("Alicka", 1);
 
     public static void main(String[] args) {
         System.out.println("Ahoj svet!");
 
 
         try {
-            SignalProtocolStore aliceStore = new MySignalProtocolStore();
-            SignalProtocolStore bobStore = new MySignalProtocolStore();
+            Encryption alickaEnc = new Encryption(ALICE_ADDRESS);
+            Encryption bobikEnc = new Encryption(BOB_ADDRESS);
 
-            PreKeyBundle alicePreKeyBundle = createAlicePreKeyBundle(aliceStore);
-            PreKeyBundle bobPreKeyBundle = createBobPreKeyBundle(bobStore);
+            alickaEnc.sendIdentityKey();
+            alickaEnc.rotateSingnedPreKey();
+            alickaEnc.addOnetimePreKeys(10);
+            bobikEnc.sendIdentityKey();
+            bobikEnc.rotateSingnedPreKey();
+            bobikEnc.addOnetimePreKeys(10);
 
-            SessionBuilder aliceSessionBuilder = new SessionBuilder(aliceStore, BOB_ADDRESS);
-            SessionBuilder bobSessionBuilder = new SessionBuilder(bobStore, ALICE_ADDRESS);
+            String alicaMsgOutCl = "Ahoj, ja som ALica";
+            byte[] alicaMsgOutCipher = alickaEnc.encrypt(BOB_ADDRESS, alicaMsgOutCl.getBytes());
 
-            SessionCipher aliceSessionCipher = new SessionCipher(aliceStore, BOB_ADDRESS);
-            SessionCipher bobSessionCipher = new SessionCipher(bobStore, ALICE_ADDRESS);
+            String bobMsgInCl = new String(bobikEnc.decrypt(ALICE_ADDRESS, alicaMsgOutCipher));
+            System.out.printf("Bob dostal sprabu '%s'\n", bobMsgInCl);
+            assert (bobMsgInCl == alicaMsgOutCl);
 
-            aliceSessionBuilder.process(bobPreKeyBundle);
-            bobSessionBuilder.process(alicePreKeyBundle);
 
-            CiphertextMessage messageForBob = aliceSessionCipher.encrypt("hey there".getBytes());
-            CiphertextMessage messageForAlice = bobSessionCipher.encrypt("sample message".getBytes());
+            String bobMsgOutCl = "Nazdar, ja som Bob";
+            byte[] bobMsgOutCipher = bobikEnc.encrypt(ALICE_ADDRESS, bobMsgOutCl.getBytes());
 
-            byte[] alicePlaintext = aliceSessionCipher.decrypt(new PreKeySignalMessage(messageForAlice.serialize()));
-            byte[] bobPlaintext = bobSessionCipher.decrypt(new PreKeySignalMessage(messageForBob.serialize()));
-
-            System.out.printf("Alicka precitala spravu '%s'\n", new String(alicePlaintext));
-            System.out.printf("Bob precitala spravu '%s'\n", new String(bobPlaintext));
+            String alicaMsgInCl = new String(alickaEnc.decrypt(BOB_ADDRESS, bobMsgOutCipher));
+            System.out.printf("Alica dostala sprabu '%s'\n", alicaMsgInCl);
+            assert (alicaMsgInCl == bobMsgOutCl);
         }
         catch (Exception e) {
             System.out.printf("Nieco je velmi zle '%s'...\n", e.getMessage());
         }
     }
-
-    //TODO zbavit sa
-    private static PreKeyBundle createAlicePreKeyBundle(SignalProtocolStore aliceStore) throws InvalidKeyException {
-        ECKeyPair aliceUnsignedPreKey   = Curve.generateKeyPair();
-        int       aliceUnsignedPreKeyId = new Random().nextInt(Medium.MAX_VALUE);
-        byte[]    aliceSignature        = Curve.calculateSignature(aliceStore.getIdentityKeyPair().getPrivateKey(),
-                aliceSignedPreKey.getPublicKey().serialize());
-
-        PreKeyBundle alicePreKeyBundle = new PreKeyBundle(1, 1,
-                aliceUnsignedPreKeyId, aliceUnsignedPreKey.getPublicKey(),
-                aliceSignedPreKeyId, aliceSignedPreKey.getPublicKey(),
-                aliceSignature, aliceStore.getIdentityKeyPair().getPublicKey());
-
-        aliceStore.storeSignedPreKey(aliceSignedPreKeyId, new SignedPreKeyRecord(aliceSignedPreKeyId, System.currentTimeMillis(), aliceSignedPreKey, aliceSignature));
-        aliceStore.storePreKey(aliceUnsignedPreKeyId, new PreKeyRecord(aliceUnsignedPreKeyId, aliceUnsignedPreKey));
-
-        return alicePreKeyBundle;
-    }
-
-    private static PreKeyBundle createBobPreKeyBundle(SignalProtocolStore bobStore) throws InvalidKeyException {
-        ECKeyPair bobUnsignedPreKey   = Curve.generateKeyPair();
-        int       bobUnsignedPreKeyId = new Random().nextInt(Medium.MAX_VALUE);
-        byte[]    bobSignature        = Curve.calculateSignature(bobStore.getIdentityKeyPair().getPrivateKey(),
-                bobSignedPreKey.getPublicKey().serialize());
-
-        PreKeyBundle bobPreKeyBundle = new PreKeyBundle(1, 1,
-                bobUnsignedPreKeyId, bobUnsignedPreKey.getPublicKey(),
-                bobSignedPreKeyId, bobSignedPreKey.getPublicKey(),
-                bobSignature, bobStore.getIdentityKeyPair().getPublicKey());
-
-        bobStore.storeSignedPreKey(bobSignedPreKeyId, new SignedPreKeyRecord(bobSignedPreKeyId, System.currentTimeMillis(), bobSignedPreKey, bobSignature));
-        bobStore.storePreKey(bobUnsignedPreKeyId, new PreKeyRecord(bobUnsignedPreKeyId, bobUnsignedPreKey));
-
-        return bobPreKeyBundle;
-    }
-
 }
